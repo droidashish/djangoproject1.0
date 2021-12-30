df2b1cebfc1a9b544dc993c1927a97b8c3f57cc6
droidashish <ashish.a4911@gmail.com>
2021-12-30 20:30:08 +0530

    Started working on bootstrap and python backend

--- sitepages/views.py
+++ sitepages/views.py
@@ -1,3 +1,14 @@
+
 from django.shortcuts import render
 
+#DataFlair #Views #TemplateInheritance
 # Create your views here.
+def home(request):
+    return render(request, 'base.html')
+
+def other(request):
+    context = {
+    'k1': 'Welcome to the Second page',
+    }
+    return render(request, 'others.html')
+
