e59ba6c8d1fb1bd1199f9e5065f7c748dbb1e7e9
droidashish <ashish.a4911@gmail.com>
2022-09-16 10:16:04 +0530

    commit1

---
+++ homepage/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse("Hello, world. my site here.")
+
+
+def second(request):
+    return HttpResponse("My Second page!")
